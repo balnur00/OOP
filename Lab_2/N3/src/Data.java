cfda96af1175f04eacddf59037420dd6f10a7aa5
Balnur <balnur00@mail.ru>
2018-09-11 11:20:35 +0600

    next

--- Lab_2/N3/src/Data.java
+++ Lab_2/N3/src/Data.java
@@ -1,10 +1,56 @@
-public class Data {
-    private double n1;
-    private double n2;
-    private int n3;
-    public Data(){ }
+import java.util.Scanner;
 
-    public void addVal(double n){
+    public class Data{
+        private int cnt = 0;
+        private double sum = 0;
+        private double maxi = -1000;
+        public Data(){
+        }
+        public void addVal(double a){
+            if(a > maxi){
+                maxi = a;
+            }
+            sum += a;
+            cnt++;
+        }
+        public int getCnt(){
+            return cnt;
+        }
+        public double findAve(){
+            return sum/cnt;
+        }
+        public double maxxi(){
+            return maxi;
+        }
+    }
+      class Analyzer{
+        double average;
+        double maxx;
+
+        public Analyzer(Data data){
+            average=data.findAve();
+            maxx=data.maxxi();
+        }
+
+    public static void main(String[] args) {
+        Data data=new Data();
+        while(true) {
+            Scanner s = new Scanner(System.in);
+            String string = s.next();
+            int ascii = (int) string.charAt(0);
+            if (ascii >= 48 && ascii <= 57) {
+                data.addVal(Double.parseDouble(string));
+            } else {
+                Analyzer analyzer = new Analyzer(data);
+                if(data.getCnt()==0){
+                    System.out.println(0);
+                    break;
+                }
+                System.out.println(analyzer.average );
+                System.out.println(analyzer.maxx );
+                break;
+            }
+        }
 
     }
 }
