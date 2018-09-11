6fec027d64a173f9b8412bcaec92281471e28b64
Balnur <balnur00@mail.ru>
2018-09-11 12:31:37 +0600

    hey

---
+++ Lab1/hackerrank/medium/1.java
@@ -0,0 +1,39 @@
+		System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+		
+		}//end of try
+		
+		catch (DoNotTerminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	}//end of main
+	static class Inner{
+		private class Private{
+			private String powerof2(int num){
+				return ((num&num-1)==0)?"power of 2":"not a power of 2";
+			}
+		}
+	}//end of Inner
+	
+}//end of Solution
+
+class DoNotTerminate { //This class prevents exit(0)
+	 
+    public static class ExitTrappedException extends SecurityException {
+
+		private static final long serialVersionUID = 1L;
+    }
+ 
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}	
+	
+
--- Lab_2/Task1/src/Student.java
+++ Lab_2/Task1/src/Student.java
@@ -1,2 +1,44 @@
 public class Student {
+    private int id;
+    private String name;
+    private int yearOfStudy = 1;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfStudy() {
+        return yearOfStudy;
+    }
+
+    public void incrYearOfStudy() {
+        yearOfStudy++;
+    }
+
+
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Balnur");
+        System.out.println(student1.getId() + " " + student1.getName() + " " + student1.getYearOfStudy());
+        System.out.print("Next year of study is:  ");
+        student1.incrYearOfStudy();
+        System.out.println(student1.getYearOfStudy());
+
+        System.out.println("===");
+
+        Student student2 = new Student(2, "Dilnaz");
+        System.out.println(student2.getId() + " " + student2.getName() + " " + student2.getYearOfStudy());
+        System.out.print("Next year of study is:  ");
+        student2.incrYearOfStudy();
+        System.out.println(student2.getYearOfStudy());
+
+    }
 }
